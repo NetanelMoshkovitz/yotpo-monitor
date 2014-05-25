71702f69c9bda396f8f59855ee17b6726205d927
Netanel Moshkovitz <nethanel@yotpo.com>
2014-05-25 14:32:12 +0300

    change procgile

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,6 @@
-worker_processes 4
+worker_processes 30
 timeout 300
+preload_app true
 
 @app_path = '/Users/netanel/Development/yotpo-workspace/yotpo-monitor'
 listen "#{@app_path}/tmp/sockets/unicorn.sock", :backlog => 64
